bb653d9f0ebc540f1ae13dd86ff0338b11baf6fd
retopara <wangzhen.ustc@outlook.com>
2018-08-12 23:34:11 +0800

    revert to first version, simple but works

--- test.py
+++ test.py
@@ -7,64 +7,23 @@ from wxbot import *
 
 class MyWXBot(WXBot):
     def handle_msg_all(self, msg):
-        """
-        This function auto saves pics/videos in group messages
-        :param msg: the message dict
-        :return: None
-        """
+        # if msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
+        #     self.send_msg_by_uid(u'hi', msg['user']['id'])
+        #     #self.send_img_msg_by_uid("img/1.png", msg['user']['id'])
+        #     #self.send_file_msg_by_uid("img/1.png", msg['user']['id'])
 
-        if msg['msg_type_id'] == 3:
-            print("msg_id = ", msg['msg_id']
-                  , " msg_type_id = ", msg['msg_type_id']
-                  , " msg_content_type = ", msg['content']['type']
-                  , " group_name = ", self.to_unicode(msg['user']['name'])
-                  )
+        print("inside handle all msg, msg_id = ", msg['msg_id'])
+        print("msg_type_id = ", msg['msg_type_id'], "msg_content_type = ", msg['content']['type'])
 
-            if msg['content']['type'] == 3:
-                print("this should be a image message, pic saved to ", self.get_msg_img(msg))
-                # print("pic saved to ./temp/", self.get_msg_img(msg['msg_id']))
+        if msg['msg_type_id'] == 3 and msg['content']['type'] == 3:
+            print("this should be a image message, pic saved to ./temp/", self.get_msg_img(msg['msg_id']))
+            # print("pic saved to ./temp/", self.get_msg_img(msg['msg_id']))
 
-            if msg['content']['type'] == 13 or msg['content']['type'] == 8:
-                print("this should be a video message, saved to ", self.get_video(msg))
+        if msg['msg_type_id'] == 3 and msg['content']['type'] == 13:
+            print("this should be a SMALL VIDEO, saved to ./temp/", self.get_video(msg['msg_id']))
 
-    def get_msg_img(self, msg):
-        """
-        获取图片消息，下载群内图片到本地分为不同群名的文件夹
-        :param msg: 消息
-        :return: 保存的本地图片文件路径
-        """
-        url = self.base_uri + '/webwxgetmsgimg?MsgID=%s&skey=%s' % (msg['msg_id'], self.skey)
-        r = self.session.get(url)
-        data = r.content
-        fn = 'img_' + msg['msg_id'] + '.jpg'
-        fp = os.path.join(os.getcwd(), msg['user']['name'].decode('utf-8'))
-        fpn = os.path.join(fp, fn)
-        if not os.path.isdir(fp):
-            os.makedirs(fp)
-        with open(fpn, 'wb') as f:
-            f.write(data)
-        return fpn
-
-    def get_video(self, msg):
-        """
-        获取视频消息，下载视频到本地
-        :param msg: 视频消息
-        :return: 保存的本地视频文件路径
-        """
-        url = self.base_uri + '/webwxgetvideo?msgid=%s&skey=%s' % (msg['msg_id'], self.skey)
-        headers = {'Range': 'bytes=0-'}
-        r = self.session.get(url, headers=headers)
-        data = r.content
-        fn = 'video_' + msg['msg_id'] + '.mp4'
-        fp = os.path.join(os.getcwd(), msg['user']['name'].decode('utf-8'))
-        fpn = os.path.join(fp, fn)
-        if not os.path.isdir(fp):
-            os.makedirs(fp)
-        with open(fpn, 'wb') as f:
-            f.write(data)
-        return fpn
-
-
+        if msg['msg_type_id'] == 3 and msg['content']['type'] == 8:
+            print("this should be a video message, saved to ./temp/", self.get_video(msg['msg_id']))
 '''
     def schedule(self):
         self.send_msg(u'张三', u'测试')
